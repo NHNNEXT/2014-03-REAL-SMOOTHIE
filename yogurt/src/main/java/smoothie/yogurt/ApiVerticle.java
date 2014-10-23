cf0a54b6c0b4c912587555e81014f8558962ad43
fourwingsy <fourwingsy@gmail.com>
2014-10-23 22:31:03 +0900

    #22 read post-body well now

--- yogurt/src/main/java/smoothie/yogurt/ApiVerticle.java
+++ yogurt/src/main/java/smoothie/yogurt/ApiVerticle.java
@@ -1,9 +1,13 @@
 package smoothie.yogurt;
 
+import java.net.URLDecoder;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.vertx.java.core.Handler;
 import org.vertx.java.core.MultiMap;
+import org.vertx.java.core.buffer.Buffer;
 import org.vertx.java.core.http.HttpServerRequest;
 import org.vertx.java.core.http.RouteMatcher;
 import org.vertx.java.core.json.JsonObject;
@@ -21,12 +25,24 @@ public class ApiVerticle extends Verticle {
 
 		matcher.get("/api/saveresult", new Handler<HttpServerRequest>() {
 			public void handle(HttpServerRequest req) {
-				handleMoves(req);
+				handleMoves(req, null);
 			}
 		});
 		matcher.post("/api/saveresult", new Handler<HttpServerRequest>() {
-			public void handle(HttpServerRequest req) {
-				handleMoves(req);
+			public void handle(final HttpServerRequest req) {
+				req.bodyHandler(new Handler<Buffer>() {
+		            @Override
+		            public void handle(Buffer buff) {
+		                Map<String, String> map = null;
+		                String contentType = req.headers().get("Content-Type");
+
+		                if ("application/x-www-form-urlencoded".equals(contentType)) {
+		                    map = getQueryMap(buff.toString()); // takes the buffer string and returns map of post params
+		                }
+
+		                handleMoves(req, map);
+		            }
+		        });
 			}
 		});
 		
@@ -45,31 +61,61 @@ public class ApiVerticle extends Verticle {
 		successMessage.putObject("error", success);
 	}
 	
-	public void handleMoves(HttpServerRequest req) {
+	public void handleMoves(HttpServerRequest req, Map<String,String> map) {
+		
+		// Get Params
         MultiMap params = req.params();
+        // Post Params
+        if (map != null)
+        	params.add(map);
+        
         JsonObject logJson = new JsonObject();
         
         String moveStr = params.get("moves");
         int moves = -1;
-        if (moveStr == null) {
-        	req.response().putHeader("content-type", "application/json");
-			req.response().end(failMessage.toString());
-        } else {
-        	moves = Integer.parseInt(moveStr);
-        	// DB 접근하여 하이스코어 알아오기
-        	req.response().putHeader("content-type", "application/json");
+        
+    	try {
+    		moves = Integer.parseInt(moveStr);
+    		if (moves < 0) {
+    			throw new NumberFormatException();
+    		}
+    		// DB 접근하여 하이스코어 알아오기
+    		req.response().putHeader("content-type", "application/json");
 			req.response().end(successMessage.toString());
-        }
-        
-        logJson.putString("time", new Date().toString());
-        logJson.putNumber("moves", moves);
-        
-        JsonObject operation = new JsonObject();
-		operation.putString("action", "save");
-		operation.putString("collection", "test");
-		operation.putObject("document", logJson);
-        // and call the event we want to use
-        vertx.eventBus().send("vertx.mongopersistor", operation);
+    	} catch (NumberFormatException nfe) {
+    		req.response().putHeader("content-type", "application/json");
+			req.response().end(failMessage.toString());
+    	} finally {
+	        logJson.putString("time", new Date().toString());
+	        logJson.putNumber("moves", moves);
+	        
+	        JsonObject operation = new JsonObject();
+			operation.putString("action", "save");
+			operation.putString("collection", "test");
+			operation.putObject("document", logJson);
+	        // and call the event we want to use
+	        vertx.eventBus().send("vertx.mongopersistor", operation);
+    	}
 		
+	}
+	
+	public static Map<String, String> getQueryMap(String query)
+	{
+	    String[] params = query.split("&");
+	    Map<String, String> map = new HashMap<String, String>();
+	    for (String param : params) {
+	        String name = param.split("=")[0];
+	        String value = "";
+
+	        try {
+	            value = URLDecoder.decode(param.split("=")[1], "UTF-8");
+	        } catch (Exception e) {
+	            System.out.println("wtf exception: " + e.getMessage());
+	        }
+
+	        map.put(name, value);
+	    }
+
+	    return map;
 	}
 }
