67499614f895a95003dd728601b339aa10210866
fourwingsy <fourwingsy@gmail.com>
2014-12-22 16:08:47 +0900

    server api change

--- yogurt/src/main/java/smoothie/yogurt/HttpVerticle.java
+++ yogurt/src/main/java/smoothie/yogurt/HttpVerticle.java
@@ -1,21 +1,4 @@
 package smoothie.yogurt;
-/*
- * Copyright 2013 Red Hat, Inc.
- *
- * Red Hat licenses this file to you under the Apache License, version 2.0
- * (the "License"); you may not use this file except in compliance with the
- * License.  You may obtain a copy of the License at:
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
- * License for the specific language governing permissions and limitations
- * under the License.
- *
- * @author <a href="http://tfox.org">Tim Fox</a>
- */
 
 import java.util.Map;
 
@@ -27,9 +10,7 @@ import org.vertx.java.core.http.RouteMatcher;
 import org.vertx.java.core.json.JsonObject;
 import org.vertx.java.platform.Verticle;
 
-/*
-This is a simple Java verticle which receives `ping` messages on the event bus and sends back `pong` replies
- */
+
 public class HttpVerticle extends Verticle {
 
 	public void start() {
@@ -41,13 +22,14 @@ public class HttpVerticle extends Verticle {
 				StringBuilder sb = new StringBuilder();
 				for (Map.Entry<String, String> header : request.headers()
 						.entries()) {
+					container.logger().info(header.getKey());
 					sb.append(header.getKey()).append(": ")
 							.append(header.getValue()).append("\n");
 				}
 				request.response().putHeader("content-type", "text/plain");
 				request.response().end(sb.toString());
 				
-				saveRequestLog(sb.toString());
+//				saveRequestLog(sb.toString());
 			}
 
 			private void saveRequestLog(String log) {
